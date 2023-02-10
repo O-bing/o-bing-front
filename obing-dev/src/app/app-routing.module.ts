0e932939649e593a5b9f7fec621936f2ac763c44
LEROY DUCARDONNOY Paul <pld.pro@hotmail.com>
2023-02-10 23:18:24 +0100

    rename+base front mainpage header & footer

--- obing-dev/src/app/app-routing.module.ts
+++ obing-dev/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BingoSearchComponent } from './bingo-search/bingo-search.component';
-import { CreditsPageComponent } from './credits-page/credits-page.component';
-import { LoginComponent } from './login/login.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { UserSearchComponent } from './user-search/user-search.component';
-import { UserComponent } from './user/user.component';
+import { BingoSearchComponent } from './components/bingo-search/bingo-search.component';
+import { CreditsPageComponent } from './components/credits-page/credits-page.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { UserSearchComponent } from './components/user-search/user-search.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
   {
--- obing-dev/src/app/app.module.ts
+++ obing-dev/src/app/app.module.ts
@@ -3,37 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainPageComponentComponent } from './main-page-component/main-page-component.component';
-import { UserComponentComponent } from './user-component/user-component.component';
-import { UserSearchComponentComponent } from './user-search-component/user-search-component.component';
-import { BingoSearchComponentComponent } from './bingo-search-component/bingo-search-component.component';
-import { LoginComponentComponent } from './login-component/login-component.component';
-import { SignUpComponentComponent } from './sign-up-component/sign-up-component.component';
-import { CreditsPageComponentComponent } from './credits-page-component/credits-page-component.component';
-import { PageNotFoundComponentComponent } from './page-not-found-component/page-not-found-component.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { CreditsPageComponent } from './credits-page/credits-page.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { LoginComponent } from './login/login.component';
-import { BingoSearchComponent } from './bingo-search/bingo-search.component';
-import { UserSearchComponent } from './user-search/user-search.component';
-import { UserComponent } from './user/user.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { CreateBingoComponent } from './create-bingo/create-bingo.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { CreditsPageComponent } from './components/credits-page/credits-page.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { LoginComponent } from './components/login/login.component';
+import { BingoSearchComponent } from './components/bingo-search/bingo-search.component';
+import { UserSearchComponent } from './components/user-search/user-search.component';
+import { UserComponent } from './components/user/user.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CreateBingoComponent } from './components/create-bingo/create-bingo.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPageComponentComponent,
-    UserComponentComponent,
-    UserSearchComponentComponent,
-    BingoSearchComponentComponent,
-    LoginComponentComponent,
-    SignUpComponentComponent,
-    CreditsPageComponentComponent,
-    PageNotFoundComponentComponent,
     PageNotFoundComponent,
     CreditsPageComponent,
     SignUpComponent,
@@ -48,7 +33,8 @@ import { CreateBingoComponent } from './create-bingo/create-bingo.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
