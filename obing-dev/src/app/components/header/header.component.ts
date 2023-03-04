862ec2ec7aba5831f6c40989418ec858f4420558
LEROY DUCARDONNOY Paul <pld.pro@hotmail.com>
2023-03-05 00:56:25 +0100

    mainpage clean

--- obing-dev/src/app/components/header/header.component.ts
+++ obing-dev/src/app/components/header/header.component.ts
@@ -1,30 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-
-document.addEventListener('DOMContentLoaded', () => {
-
-  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
-  if ($navbarBurgers.length > 0) {
-
-    $navbarBurgers.forEach( el => {
-      el.addEventListener('click', () => {
-
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        el.classList.toggle('is-active');
-        if ($target != null)
-        {
-          $target.classList.toggle('is-active');
-        }
-
-      });
-    });
-  }
-
-});
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
