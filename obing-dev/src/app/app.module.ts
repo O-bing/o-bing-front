b11d5e95df1c3c2378ee24a71c21c294b5cec3df
AL1062886 <paul.leroyducardonnoy@cgi.com>
2023-06-19 10:54:08 +0200

    testing pwa handling

--- obing-dev/src/app/app.module.ts
+++ obing-dev/src/app/app.module.ts
@@ -31,6 +31,7 @@ import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from '../environments/environment';
 import { AuthService } from './@shared/services/auth/auth.service';
+import { ServiceWorkerModule } from '@angular/service-worker';
 @NgModule({
   declarations: [
     AppComponent,
@@ -65,7 +66,13 @@ import { AuthService } from './@shared/services/auth/auth.service';
     AngularFireStorageModule,
     AngularFireDatabaseModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [AuthService],
   bootstrap: [AppComponent]
