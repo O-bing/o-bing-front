03879c0b827d7e46471e039df92ce3e7f667f6ee
AL1062886 <paul.leroyducardonnoy@cgi.com>
2023-08-28 11:13:28 +0200

    console log

--- obing-dev/src/app/components/bingo-user-list/bingo-user-list.component.ts
+++ obing-dev/src/app/components/bingo-user-list/bingo-user-list.component.ts
@@ -33,7 +33,6 @@ export class BingoUserListComponent implements OnInit {
           this.bingoService.getAllBingos().subscribe(bingoArray=>{
             bingoArray.forEach(bingo=>{
               if (bingo.owner==this.currentUser.uid){
-                console.log(bingo)
                 if (bingo.title!.length>15){
                   bingo.displayName = bingo.title!.slice(0,15)+"..."
                 }else{
