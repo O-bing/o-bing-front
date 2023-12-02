4ebdfbd31c8a5698d0ac63c744e95fc01f6faa36
LEROY DUCARDONNOY Paul <pld.pro@hotmail.com>
2023-12-02 12:48:14 +0100

    added a node_modules patcher for local developpment

---
+++ .github/workflows/package-patcher-dev.js
@@ -0,0 +1,61 @@
+const {readFile, writeFile, readdirSync, promises: fsPromises} = require('fs');
+
+const packagePath = '../../obing-dev/node_modules/@angular/fire/compat/firestore/interfaces.d.ts'
+
+const dirContents = readdirSync('../../obing-dev/');
+
+readFile(packagePath, 'utf-8', function (err, contents) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const toReplace = `
+export interface DocumentSnapshotExists<T> extends firebase.firestore.DocumentSnapshot {
+    readonly exists: true;
+    data(options?: SnapshotOptions): T;
+}
+export interface DocumentSnapshotDoesNotExist extends firebase.firestore.DocumentSnapshot {
+    readonly exists: false;
+    data(options?: SnapshotOptions): undefined;
+    get(fieldPath: string | FieldPath, options?: SnapshotOptions): undefined;
+}
+export declare type DocumentSnapshot<T> = DocumentSnapshotExists<T> | DocumentSnapshotDoesNotExist;
+export interface QueryDocumentSnapshot<T> extends firebase.firestore.QueryDocumentSnapshot {
+    data(options?: SnapshotOptions): T;
+}
+export interface QuerySnapshot<T> extends firebase.firestore.QuerySnapshot {
+    readonly docs: QueryDocumentSnapshot<T>[];
+}
+export interface DocumentChange<T> extends firebase.firestore.DocumentChange {
+    readonly doc: QueryDocumentSnapshot<T>;
+}`
+
+  const replacer = `
+export interface DocumentSnapshotExists<T> extends firebase.firestore.DocumentSnapshot<T> {
+    readonly exists: true;
+    data(options?: SnapshotOptions): T;
+}
+export interface DocumentSnapshotDoesNotExist extends firebase.firestore.DocumentSnapshot {
+    readonly exists: false;
+    data(options?: SnapshotOptions): undefined;
+    get(fieldPath: string | FieldPath, options?: SnapshotOptions): undefined;
+}
+export declare type DocumentSnapshot<T> = DocumentSnapshotExists<T> | DocumentSnapshotDoesNotExist;
+export interface QueryDocumentSnapshot<T> extends firebase.firestore.QueryDocumentSnapshot<T> {
+    data(options?: SnapshotOptions): T;
+}
+export interface QuerySnapshot<T> extends firebase.firestore.QuerySnapshot<T> {
+    readonly docs: QueryDocumentSnapshot<T>[];
+}
+export interface DocumentChange<T> extends firebase.firestore.DocumentChange<T> {
+    readonly doc: QueryDocumentSnapshot<T>;
+}`
+
+  // 👇️ match string case-insensitively 👇️
+  const replaced = contents.replace(toReplace, replacer);
+
+  writeFile(packagePath, replaced, 'utf-8', function (err) {
+    console.log(err);
+  });
+});
