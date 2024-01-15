09410d8197c853343f410542865ff2138895cb72
AL1062886 <paul.leroyducardonnoy@cgi.com>
2024-01-15 13:48:06 +0100

    activatedroute subscribe test def ok

--- obing-dev/src/app/components/bingo-card/bingo-card.component.spec.ts
+++ obing-dev/src/app/components/bingo-card/bingo-card.component.spec.ts
@@ -1,10 +1,13 @@
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from 'src/environments/environment';
+import { MatLegacyDialog as MatDialog } from '@angular/material/legacy-dialog';
+import { ActivatedRoute, Data, Router } from '@angular/router';
 
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BingoCardComponent } from './bingo-card.component';
+import { of } from 'rxjs';
 
 describe('BingoCardComponent', () => {
   let component: BingoCardComponent;
@@ -14,9 +17,18 @@ describe('BingoCardComponent', () => {
     await TestBed.configureTestingModule({
       imports: [
         AngularFireModule.initializeApp(environment.firebase),
-        AngularFirestoreModule
+        AngularFirestoreModule        
       ],
-      declarations: [ BingoCardComponent ]
+      declarations: [ BingoCardComponent ],
+      providers:[
+        {provide:Router, useValue:{}},
+        {provide: MatDialog, useValue: {}},
+        {provide: ActivatedRoute, useValue: {
+          paramMap: of(new Map(Object.entries({
+            id: '34'
+        })))
+        }}
+      ]
     })
     .compileComponents();
   });
